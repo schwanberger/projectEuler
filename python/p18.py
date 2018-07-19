d8cfb1e870381fab2193298abd075d65e05671eb
schwanberger <thomas@schwanberger.dk>
2018-07-19 16:04:20 +0200

    Problem 18 brute force method added.

--- python/p18.py
+++ python/p18.py
@@ -28,49 +28,51 @@
 # 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 # 
 # NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
-data = [ 
-[3],
-[7, 4],
-[2, 4, 6],
-[8, 5, 9, 3]
-]
+# data = [ 
+# [3],
+# [7, 4],
+# [2, 4, 6],
+# [8, 5, 9, 3]
+# ]
 
-# data = [
-# [75],
-# [95, 64],
-# [17, 47, 82],
-# [18, 35, 87, 10],
-# [20, 04, 82, 47, 65],
-# [19, 01, 23, 75, 03, 34],
-# [88, 02, 77, 73, 07, 63, 67],
-# [99, 65, 04, 28, 06, 16, 70, 92],
-# [41, 41, 26, 56, 83, 40, 80, 70, 33],
-# [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
-# [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
-# [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
-# [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
-# [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-# [04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23],
-#     ]
+data = [
+[75],
+[95, 64],
+[17, 47, 82],
+[18, 35, 87, 10],
+[20, 04, 82, 47, 65],
+[19, 01, 23, 75, 03, 34],
+[88, 02, 77, 73, 07, 63, 67],
+[99, 65, 04, 28, 06, 16, 70, 92],
+[41, 41, 26, 56, 83, 40, 80, 70, 33],
+[41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+[53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+[70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+[91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+[63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+[04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23],
+     ]
 
-print data
+i_MAX = len(data)
+j_MAX = len(data)
 
-i_MAX = len(data) - 1
-j_MAX = len(data) - 1
+derpList = set()
 
-tList = []
 
-def brutus(i, j):
-    global tList
+def brutus(i, j, result):
+    print (i, j)
+#     if j == j_MAX:
+#         derpList.append(result)
+#         return result
     
-    if i < i_MAX and j < j_MAX:
-        print i, j
-        
-        return brutus(i + 1, j) + brutus(i+1,j+1)
-        
-    else:
-        print i, j 
-        return True,True
- 
-         
-print brutus(0, 0)
+    if i == i_MAX:
+        derpList.add(result)
+        # print result
+        return result
+    
+    return brutus(i + 1, j, result + data[i][j]) + brutus(i + 1, j + 1, result + data[i][j])
+
+
+brutus(0, 0, 0)
+print max(derpList)
+
