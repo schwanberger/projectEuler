417b3e662c18123435d94c2805bda160c91904f9
Thomas Schwanberger <thomas@schwanberger.dk>
2018-07-23 11:08:51 +0200

    Added dynamical bottom-up solution

--- python/p31.py
+++ python/p31.py
@@ -10,17 +10,43 @@
 # 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 # How many different ways can £2 be made using any number of coins?
 
-# Recursive solution, could be optimized by memoization
+
+def memoize(f):
+    memo = {}
+
+    def helper(n, m):
+        if (n, m) not in memo:            
+            memo[n, m] = f(n, m)
+        return memo[n, m]
+
+    return helper
+
+
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-
-def count(n, m):
+#Dynamical programming, top-down approach by analysing sub-structure and formulating a recursion that fits
+@memoize
+def recCount(n, m):
     if n < 0 or m < 0:
         return 0
     if n == 0:  
         return 1
  
-    return count(n, m - 1) + count(n - coins[m], m)
+    return recCount(n, m - 1) + recCount(n - coins[m], m)
 
 
-print count(200, 7)
+print recCount(200, 7)
+
+#Dynamical programming, bottom up approach derived from recursion above.
+def iteCount(n):
+    count = [0] * (n + 1)
+    count[0] = 1
+    for i in xrange(len(coins)):
+        for j in xrange(coins[i], n + 1):
+            count[j] += count[j - coins[i]]
+            
+    return count[n]
+
+
+print iteCount(200)
+print iteCount(10000)
